09c690efb5605274269af38aaabeac7d56271393
mshannah <moh_shannah@hotmail.com>
2022-06-02 14:07:44 +0400

    maven test fix

--- src/test/java/aim/apps/ebs/ExampleResourceTest.java
+++ src/test/java/aim/apps/ebs/ExampleResourceTest.java
@@ -11,7 +11,7 @@ public class ExampleResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given().when().get("/hello").then().statusCode(200).body(is("Hello RESTEasy"));
+        given().when().get("/hello").then().statusCode(200).body(is("Hello RESTEasy 2.0"));
     }
 
 }
